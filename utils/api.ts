db2b9c7fa29942df5f88ec89ac38bbd7eb9ce29f
João Vítor <33376280+joaogelado@users.noreply.github.com>
2022-07-17 19:39:22 -0300

    fix client requesting to wrong server

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? `${process.env.VERCEL_URL}/api` : "http://localhost:3000/api"
-})
+    baseURL:
+        process.env.NODE_ENV === "production"
+            ? `${process.env.VERCEL_URL != undefined ?? ""}/api` // little check to see if it is on server (no relative URLs) or client (relative URLs)
+            : "http://localhost:3000/api",
+});
