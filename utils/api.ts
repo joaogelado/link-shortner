f54445edf9ad04589da09aad74a0eb916f4e8f8d
João Vítor <33376280+joaogelado@users.noreply.github.com>
2022-07-17 22:34:03 -0300

    make PUBLIC_API_URL be defined for all env and both server and client accessible

--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:
-        process.env.NODE_ENV === "production"
-            ? `${process.env.DEPLOY_URL ?? ""}/api` // little check to see if it is on server (no relative URLs) or client (relative URLs)
-            : "http://localhost:3000/api",
+    baseURL: process.env.API_URL,
 });
