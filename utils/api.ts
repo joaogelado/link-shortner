41da0617c4f0de85fc77afa8e8910bb17c99ce4a
João Vítor <33376280+joaogelado@users.noreply.github.com>
2022-07-17 22:19:03 -0300

    trying all things to get this to work on prod v6

--- utils/api.ts
+++ utils/api.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
     baseURL:
         process.env.NODE_ENV === "production"
-            ? `${process.env.VERCEL_URL ?? ""}/api` // little check to see if it is on server (no relative URLs) or client (relative URLs)
+            ? `${process.env.DEPLOY_URL ?? ""}/api` // little check to see if it is on server (no relative URLs) or client (relative URLs)
             : "http://localhost:3000/api",
 });
