a39375bc4177269a570814e71b9e777b9e2354d0
João Vítor <33376280+joaogelado@users.noreply.github.com>
2022-07-19 10:04:57 -0300

    prod fix v592

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
-        ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
-        : "http://localhost:3000/api",
+    baseURL: !process.env.VERCEL_URL ? "/api" : process.env.VERCEL_URL + "/api",
 });
