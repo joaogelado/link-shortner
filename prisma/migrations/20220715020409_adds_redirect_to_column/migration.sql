85433bdfdd1c657d29ddd6fd3130b7ccf1a8f25f
JV <33376280+joaogelado@users.noreply.github.com>
2023-03-13 13:31:09 -0300

    change to mysql

--- prisma/migrations/20220715020409_adds_redirect_to_column/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `redirect_to` to the `links` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "links" ADD COLUMN     "redirect_to" TEXT NOT NULL;
--- prisma/migrations/20220806195727_add_other_og_and_twitter_tags/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "links" ADD COLUMN     "og_description" TEXT,
-ADD COLUMN     "og_image" TEXT,
-ADD COLUMN     "og_locale" TEXT,
-ADD COLUMN     "og_site_name" TEXT,
-ADD COLUMN     "og_type" TEXT,
-ADD COLUMN     "og_url" TEXT,
-ADD COLUMN     "og_video" TEXT,
-ADD COLUMN     "twitter_card" TEXT;
